a52cc6f6be499ac6f2445d3ff75086fb12ac1d6e
Dominic <dominicmph@gmail.com>
2021-03-09 18:04:51 -0800

    final :)

--- src/main/resources/static/js/runningblock.js
+++ src/main/resources/static/js/runningblock.js
@@ -2,20 +2,38 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
 function jump(){
-    if(character.classList == "animate"){return}
+    if(character.classList.contains("animate")){return}
     character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
     },300);
 }
+var paused = false;
+
+block.classList.add("blockanimate");
 var checkDead = setInterval(function() {
+    if (paused) {
+        return;
+    }
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+
+        block.classList.remove("blockanimate");
         block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";
+
+        paused = true;
+        setTimeout(function() {
+            console.log("test");
+            alert("Game Over. score: "+Math.floor(counter/100));
+            counter=0;
+            block.style.left = "500px";
+            block.classList.add("blockanimate");
+            block.style.animation = "";
+            paused = false;
+        }, 500);
+
+
     }else{
         counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
